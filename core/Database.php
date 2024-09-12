df33b012179957fab1a3f1ca7872ddce205d9cc5
Cerbaro-R <cerbarorafel1@gmail.com>
2024-09-11 21:34:38 -0300

    configuração arquivo database

---
+++ core/Database.php
@@ -0,0 +1,15 @@
+<?php
+
+class Database {
+
+    private static $connection;
+
+    public static function getConnection() {
+        if (!self::$connection) {
+            self::$connection = new PDO('mysql:host=localhost;dbname=aluguel_carros', 'root', 'root');
+
+            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$connection;
+    }
+}
