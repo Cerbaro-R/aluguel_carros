01a7799421653eaa856eb15a710ee7efcb89f947
Cerbaro-R <cerbarorafael1@gmail.com>
2024-09-29 23:43:06 -0300

    ajuste do front da parte de clientes

--- views/carros/edit.php
+++ views/carros/edit.php
@@ -18,7 +18,7 @@
         <div class="collapse navbar-collapse" id="navbarAdmin">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Painel</a>
+              <a class="nav-link active" aria-current="page" href="/">Painel</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Perfil</a>
