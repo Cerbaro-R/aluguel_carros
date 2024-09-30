344f9ecd2be29a9fc068bf071507befb2d27e5ac
Cerbaro-R <cerbarorafael1@gmail.com>
2024-09-29 23:33:09 -0300

    ajuste de front edit carros, porém ainda não esta correto

--- views/carros/index.php
+++ views/carros/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Carros</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="public/css.css">
+    <link rel="stylesheet" href="../public/css.css">
 </head>
 <body>
     <!--Navbar -->
@@ -18,7 +18,7 @@
         <div class="collapse navbar-collapse" id="navbarAdmin">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Painel</a>
+              <a class="nav-link active" aria-current="page" href="/">Painel</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Perfil</a>
